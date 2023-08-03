f1b8c5a80698b7d2d18af1a9b9e89462692e6979
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-03 17:23:09 -0600

    feat: change color en h3

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,11 @@ console.log("Página en construcción con Patricio Estrella");
 function clickPatricio() {
     alert("La mayonesa es un instrumento?");
 }
+
+function changeColor( elementHtml, color ) {
+    // alert("Que emoción, voy a cambiar mi color")
+    console.log(elementHtml);
+    console.log(color);
+    
+    elementHtml.style.color = color;
+}
