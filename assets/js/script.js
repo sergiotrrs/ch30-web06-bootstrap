b7d37b7242304f20007d36d2d3d77dab8f9df339
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-04 11:05:19 -0600

    ci: reset colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,22 @@ function changeColor( elementHtml, color ) {
     elementHtml.style.color = color;
 }
 
-function changeColorWithPromt( elementHtml) {
+function changeColorWithPrompt( elementHtml) {
     const color = prompt("Dime el color en inglés", "yellow");
     elementHtml.innerHTML = "<h3>Ahora soy <em>" + color + "</em> </h3>";
     changeColor( elementHtml, color );
+}
+
+function resetColor(){
+    const refRedColor = document.getElementById("red-color");
+    const refGreenColor = document.getElementById("green-color");
+    const refPurpleColor = document.getElementById("purple-color");
+    const refUserColor = document.getElementById("user-color");
+
+    // refRedColor.style.color = "black";
+    changeColor(refRedColor, 'black');
+    changeColor(refGreenColor, 'black');
+    changeColor(refPurpleColor, 'black');
+    changeColor(refUserColor, 'black');
+
 }
