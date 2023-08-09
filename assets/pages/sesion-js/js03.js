4c9bc8abf6166cc08f2e38ee5b74d1869a45fd13
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-09 11:05:26 -0600

    ci: declaración de bloque

--- assets/pages/sesion-js/js03.js
+++ assets/pages/sesion-js/js03.js
@@ -1,3 +1,52 @@
 console.log("Sesión JS 03 Control de flujo");
 console.log("No olvidaremos el reto a Ed");
 
+//--------------- Declaración de bloque de código ----------------
+/*
+ Sintaxis:
+         {
+
+          }
+ Las variables declaradas con let y const
+ solo tendrán alcance(scope) dentro del bloque
+ o bloques anidados.
+
+ Si la variable en el bloque, tiene el mismo nombre
+ que una variable fuera del bloque, se le da prioridad
+ a la varible dentro del bloque.
+*/
+// let y const no se pueden redeclarar
+// let firstname = "Lalo";
+// const lastname = "García";
+var age = "24";
+
+let firstname = "Sergio";
+const lastname = "Torres";
+var age = "39";
+
+{
+  let firstname = "Mau";
+  const lastname = "Peniche";
+  var age = "26";
+  const colorFavorito = "azul";
+  var comidaFavorita = "pozole";
+
+  console.log(`${firstname} ${lastname}`); // mau peniche
+  console.log(age); // 26
+  console.log(colorFavorito); // azul
+  console.log(comidaFavorita); // pozole
+  {
+    const colorFavorito = "morado";
+    console.log(colorFavorito); // morado
+    console.log(firstname); // mau
+    {
+      console.log(colorFavorito); // morado
+      console.log(firstname); // mau
+    }
+  }
+}
+
+console.log(`${firstname} ${lastname}`); // sergio torres
+console.log(age); // 26
+//console.log( colorFavorito ); // colorFavorito no está definido
+console.log(comidaFavorita); // pozole
