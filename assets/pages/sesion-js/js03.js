d37a12730c1705931c64b9f642782fb996709193
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-09 11:22:30 -0600

    ci: teoría de if-else

--- assets/pages/sesion-js/js03.js
+++ assets/pages/sesion-js/js03.js
@@ -50,3 +50,40 @@ console.log(`${firstname} ${lastname}`); // sergio torres
 console.log(age); // 26
 //console.log( colorFavorito ); // colorFavorito no está definido
 console.log(comidaFavorita); // pozole
+
+// ----------------- Condicional if-else --------------------
+/*
+ Ejecuta una sentencia si una condición específica es
+ evaluada como verdadera.
+
+ Sintaxis:
+    if ( condición ) sentencia;
+
+    if ( condición ) {
+        sentencias;
+    }
+
+    if ( condición ) sentencia;
+    else sentencia_si_condición_es_falsa;
+
+    if ( condición ) {
+        sentencias;
+    }
+    else {
+        sentencias;
+    }
+
+    if (condición) sentencia;
+    else if (condicion 2) sentencia;
+         else sentencia_si_condicion2_es_falsa;
+
+
+    if ( condicion1 ) sentencias;
+    else if ( condicion2 ) sentencia;
+    else if ( condicion3 ) sentencia;
+    else if ( condicion4 ) sentencia;
+     .....
+    else if ( condicionN ) sentencia;
+    else condición;   
+
+*/
