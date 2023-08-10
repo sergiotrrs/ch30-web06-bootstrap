f8ac028d424313c37025f941ccaeb75b62a4bcca
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-10 10:28:56 -0600

    ci: Iterando con for of

--- assets/pages/sesion-js/js04.js
+++ assets/pages/sesion-js/js04.js
@@ -25,9 +25,47 @@ console.table( personasEnCh30 );
 
 
 // ------------- Iterando todos los elementos
+/*
+const personasEnCh30 = [ 
+    [ "Luis", "Allan" , "Anneth"  , "Maryluz" ] ,  indice 0, ByteMe(404) 
+    [ "Ed"  , "Jimena", "Marifer" , "Leo"    , "Alejandro" ] ,   indice 1, PerryCode 
+    [ "Lu"  , "Leo"   , "Daniel"  , "Gina"    ]     indice 2, BugBusters 
+];
+*/ 
 
-for (let equipo = 0; equipo < personasEnCh30.length /*filas*/; equipo++) {
-    for (let persona = 0; persona <  personasEnCh30[ equipo ].length; persona++) {
-        console.log( personasEnCh30[ equipo ][ persona ] );        
-    }    
+console.log( personasEnCh30[0][2]  ); // Anneth
+
+// Este for itera sobre los equipos integradores
+for (let equipo = 0; equipo <  personasEnCh30.length  ; equipo++) {  
+    console.log(`Equipo ${equipo} : ${personasEnCh30[equipo]}` )  ;
+    
+    // Este for itera sobre las personas
+    for( let persona = 0; persona < personasEnCh30[equipo].length ; persona++ ){
+     console.log(`Participante: ${ personasEnCh30[equipo][ persona ] }`);
+    } 
 }
+
+// ----------------------- Uso de for... of ------------------
+/* Ejecuta una sentencia por cada elemento de un objeto iterable(array, colección, string).
+Sintaxis:
+    for (const iterator of object) {
+    
+    }
+*/
+console.table( personasEnCh30 ); 
+
+const myPet = "Kraken";
+for (const character of myPet  ){
+    console.log( character );
+}
+
+for (const equipo of personasEnCh30 ) {
+    console.log( equipo ); // Arreglo de equipos
+    for (const persona of equipo) {
+        console.log( persona ); //  Persona de cada equipo
+    }
+}
+
+
+ 
+
