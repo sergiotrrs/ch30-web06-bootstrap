d2b95946764986d8e3c635c9e43b55b0945f3299
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-14 14:34:38 -0600

    ci: desestructuración

--- assets/pages/sesion-js/js05.js
+++ assets/pages/sesion-js/js05.js
@@ -143,10 +143,108 @@ const occupation = person.occupation ||  "Software Enginner"; // Software Enginn
 
 console.log(`Ocupación: ${ occupation }`); // Software Enginner
 
+// --------------- Comparación Estricta -------------------------
+/*
+ === Estrictamente igual, se debe comparar el valor y el tipo de dato
+ !== Estrictamente diferente, debe ser diferente en valor y tipo
+
+*/
+
+console.log( "23" === 23 ); // false 
+console.log( "true" === true ); // false
+console.log( "true" == true ); // false NaN == 1
+ console.log( "45" !== 45 ); // true
 
 
+// ------- Operador de desestructuración ( Destructuring ) -------
+/*
+ Es una expresión en JS que hace posible extraer datos de
+ arreglos u objetos y nos ayuda a realizar asignaciones más
+ complejas.
+
+*/
+let dataA = 10;
+let dataB = 20;
+console.log(`a: ${dataA}, b: ${dataB}`); // 10, 20
+// Intercambiar los datos -> A:20 y B :10
+/*let temp = dataA;
+dataA = dataB;
+dataB = temp;*/
+
+[ dataA, dataB  ] = [ dataB, dataA ];
+
+console.log(`a: ${dataA}, b: ${dataB}`); // 10, 20
+
+const car = {
+  model : "Vento",
+  year : 2023,
+  brand : "VW",
+  color: "red",
+  price : 320_000,
+  additionalInfo : {
+    bluetooth : true,
+    cruiseControl : true,
+    gps: false,
+  }
+}
+
+// console.log(`Modelo : ${ car.model }`); // Vento
+// console.log(`Año : ${ car.year}`); // 2023
+// console.log(`Modelo : ${ car.model }, Año : ${ car.year}`); // Vento , 2023
+
+// const carModel = car.model;
+// const carYear = car.year;
+const {  year, model : carModel, additionalInfo :{ bluetooth} } = car;
+
+console.log(`Modelo : ${ carModel }, Año : ${ year}`); // Vento , 2023
+// console.log(`Tiene bluetooth: ${car.additionalInfo.bluetooth ? "sí":"no"}` );
+// console.log(`Tiene bluetooth: ${additionalInfo.bluetooth ? "sí":"no"}` );
+console.log(`Tiene bluetooth: ${ bluetooth ? "sí":"no"}` );
 
 
+// ------------------  ejercicio ------------------------
+/*
+   Hacer una función que realice la división de 2 números
+   y entregue (return) en entero el cociente y residuo.
+
+   Llamar a la función y mostrar en la consola el resultado.
+
+*/
+const num1 = 9;
+const num2 = 4;
+
+function div (num1, num2) {
+    let cociente = parseInt (num1 / num2); // 2.5 -> 2 
+    let residuo = num1 % num2;
+    return {cociente , residuo};   // {cociente: cociente , residuo: residuo }
+
+}
+// console.log(`división: ${div(num1, num2).cociente}, residui: ${div(num1, num2).residuo}`);
+
+function printResults( num1, num2 ) {
+  let {cociente, residuo} = div(num1, num2);
+  console.log(`división: ${cociente}, residuo: ${residuo}`);  
+}
+
+printResults( 23, 3);
+printResults( 4, 2);
+printResults( 100, 45);
+
+//-------------------------------------------------------------------
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+  
+// const lowToday = LOCAL_FORECAST.today.low;
+// const highToday = LOCAL_FORECAST.today.high;
+const { today:{ low : lowToday   , high : highToday}  } = LOCAL_FORECAST;
+console.log( lowToday, highToday);
+// Only change code above this line
 
 
 
