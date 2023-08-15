1263104517d5e474d0751abcbdf30735686cbcb6
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-15 11:31:42 -0600

    ci: uso de addEvenListener

--- assets/pages/sesion-js/js06-form.js
+++ assets/pages/sesion-js/js06-form.js
@@ -1,1 +1,17 @@
 console.log("JS06 - Formulario");
+
+// Obtenemos la referencia del formulario
+// const registerForm = document.getElementById("registerForm");
+const registerForm = document.forms["registerForm"];
+
+// registerForm.addEventListener( "click", fncCallback  );
+registerForm.addEventListener( "submit", ( event )=>{
+    event.preventDefault(); // omitir las acciones por default
+    // console.log( event ); datos del click
+    const email = registerForm.elements["email"];
+    const birthdate = registerForm.elements["birthdate"];
+    console.log(email.value , birthdate.value);
+
+
+
+}  );
