c8f939b5a70cab7d1a3ae170c4c289a16d0289c4
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-16 10:54:41 -0600

    ci: uso de settimeout

--- assets/pages/sesion-js/js07-asincronia.js
+++ assets/pages/sesion-js/js07-asincronia.js
@@ -15,3 +15,33 @@ const tercerPaso = () =>{
 primerPaso();
 segundoPaso();
 tercerPaso();
+
+// --------------------Programación Asíncrona ----------------------------
+/*
+   setTimeout()
+
+   Establece un temporizador que ejecuta una función de callback
+   una vez que expire el temporizador
+
+   sintaxis:
+     setTimeout( fncCallback, tiempo_ms, argumentos_fnc );
+     setTimeout( ()=>{}  , tiempo_ms );
+
+ */
+
+const saludo = ( name ) => console.log(`Hola ${name}`);
+
+const pasoAsincrono = ( name ) =>{
+    // setTimeout(  saludo, 5000, name  )
+    // setTimeout (  ( person )=> console.log(`Hola ${person}`), 5000, name  );
+    setTimeout (  ()=> console.log(`Hola ${ name }`), 5000 );
+};
+
+console.log("*******************");
+primerPaso(); // 01- Inicio del mi programa
+pasoAsincrono("Baby Yoda multiverso"); // 5s -> Hola Baby Yoda
+tercerPaso(); // 03- Fin de mi programa
+
+
+
+
