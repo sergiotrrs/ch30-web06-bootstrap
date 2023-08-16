ad702058d72d50cc71daf12ac6d8cacbf74215ef
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-16 10:29:30 -0600

    ci: programación secuencias

---
+++ assets/pages/sesion-js/js07-asincronia.js
@@ -0,0 +1,17 @@
+console.log("Sesión JS 07 Asincronía");
+
+//------------------- Programación sincrónica (secuencial)--------------
+
+const primerPaso = () =>{
+    console.log("01- Inicio del mi programa");
+}
+const segundoPaso = () =>{
+    console.log("02- Desarrollo del mi programa");
+}
+const tercerPaso = () =>{
+    console.log("03- Fin de mi programa");
+}
+
+primerPaso();
+segundoPaso();
+tercerPaso();
