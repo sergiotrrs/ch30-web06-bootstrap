7fec066e92c2c86f255eb01d48799a13b3ab6d22
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-17 12:57:59 -0600

    ci: solicitud POST

--- assets/pages/sesion-js/js08-object.js
+++ assets/pages/sesion-js/js08-object.js
@@ -11,4 +11,22 @@ registerForm.addEventListener( "submit", ( event )=>{
         password : registerForm.elements["password"].value,        
     }
     console.table(user);
+    postUser(user);
+    // debemos convertir el objeto user a formato JSON
+    localStorage.setItem("user",  JSON.stringify( user ) ); // key, value
 }  );
+
+const postUser = async ( userData) =>{
+    const url= "https://reqres.in/api/register";
+    userData.email = "eve.holt@reqres.in"; // se requiere este email.
+
+    // la respuesta de Fetch lo recibimos en formato JSON
+    const responseJSON = await fetch( url , {
+        method: "POST", // POST, PUT, DELETE, GET
+        body: JSON.stringify( userData) , // datos del usuario
+        headers: { "Content-Type" : "application/json" },
+    });
+    const response = await responseJSON.json(); // convertir de JSON a Object JS
+    console.log(response);
+    response.id && alert( `${response.id}: ${response.token} ` );
+}
