8034c83c785a81265b2543c1ab444ee80d6fec41
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-17 12:17:44 -0600

    ci: lectura del formulario en JS

--- assets/pages/sesion-js/js08-object.js
+++ assets/pages/sesion-js/js08-object.js
@@ -1,1 +1,14 @@
-console.log("Sesión js08 - OOP");
+console.log(`Sesión js08 - OOP`);
+
+const registerForm = document.forms["registerForm"];
+
+registerForm.addEventListener( "submit", ( event )=>{
+    event.preventDefault(); 
+
+    const user = {
+        fullname: registerForm.elements["fullname"].value, 
+        email : registerForm.elements["email"].value, 
+        password : registerForm.elements["password"].value,        
+    }
+    console.table(user);
+}  );
