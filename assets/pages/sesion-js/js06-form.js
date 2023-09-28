d97e6b04d99d48eaad512e135221705f35b1ea90
Sergio Torres <sergio.torres@idr.edu.mx>
2023-09-28 04:27:27 -0600

    feat: solicitudes con JWT en el header

--- assets/pages/sesion-js/js06-form.js
+++ assets/pages/sesion-js/js06-form.js
@@ -3,96 +3,138 @@ console.log("JS06 - Formulario");
 // Obtenemos la referencia del formulario
 const registerForm = document.forms["registerForm"];
 
-// registerForm.addEventListener( "click", fncCallback  );
-registerForm.addEventListener( "submit", async ( event )=>{
-    event.preventDefault(); // omitir las acciones por default
+registerForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-    const user = {
-        email : registerForm.elements["email"].value, // string
-        password : registerForm.elements["password"].value, // string
-    }
+  const user = {
+    email: registerForm.elements["email"].value,
+    password: registerForm.elements["password"].value,
+  };
 
-    await getProducts();
-    //await postUser(user);
-    await postProduct()  
-}  );
+  await postUser(user);
+});
 
-const postUser = async ( userData) =>{
-  const url= "http://localhost:8080/login";
-  
-  const responseJSON = await fetch( url , {
-      method: "POST", // POST, PUT, DELETE, GET
-      body: JSON.stringify( userData) , // datos del usuario
-      headers: { "Content-Type" : "application/json" },
+const postUser = async (userData) => {
+  const url = "http://localhost:8080/login";
+  console.log(userData);
+  const response = await fetch(url, {
+    method: "POST", // POST, PUT, DELETE, GET
+    body: JSON.stringify(userData),
+    headers: { "Content-Type": "application/json" },
   });
-  const response = await responseJSON.json(); 
-  console.log(response);  
 
-}
+  for (let [key, value] of response.headers) {
+    console.log(`${key} = ${value}`);
+  }
 
-const getProducts = async ( ) =>{
-  const url= "http://localhost:8080/api/v1/products";
-  
-  const responseJSON = await fetch( url );
-  const response = await responseJSON.json(); 
-  console.log(response);  
+  const jwt = await response.json();
+  console.log(jwt);
+  jwt != null && localStorage.setItem("token", jwt.token);
+};
 
-}
+/**
+ * No se requiere token para esta solicitud
+ */
+const getProducts = async () => {
+  const url = "http://localhost:8080/api/v1/products";
 
-const postProduct = async ( productData) =>{
-  const url= "http://localhost:8080/api/v1/products";
-  
- productData = {
-  firstName: "Antonio",
-  lastName: "M",
-  email: "antonio3@gmail.com",
-  password: "123"
-}
+  const responseJSON = await fetch(url);
+  console.log(responseJSON.status);
+  const response = await responseJSON.json();
+  console.log(response);
+};
 
-  const responseJSON = await fetch( url , {
-      method: "POST", 
-      body: JSON.stringify( productData) , 
-      headers: { "Content-Type" : "application/json" },
+/**
+ * No se requiere token para esta solicitud
+ */
+const postProduct = async (productData) => {
+  const url = "http://localhost:8080/api/v1/products";
+
+  const randomId = getRandomInt(10_000); // para generar email aleatorio
+  productData = {
+    firstName: `Patricio${randomId}`,
+    lastName: "Estrella",
+    email: `patricio${randomId}@gmail.com`,
+    password: "123",
+  };
+
+  const responseJSON = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(productData),
+    headers: { "Content-Type": "application/json" },
   });
-  const response = await responseJSON.json(); 
-  console.log(response);  
+  const response = await responseJSON.json();
+  console.log(response);
+};
 
-}
+/**
+ * Se requiere tener un token
+ */
+const getCustomers = async () => {
+  const url = "http://localhost:8080/api/v1/customers";
+  const token = localStorage.getItem("token");
+  const response = await fetch(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  console.log(response.status);
+  const customers = await response.json();
+  console.log(customers);
+};
 
+/**
+ * Se requiere tener un token
+ */
+const postOrder = async () => {
+  const url = "http://localhost:8080/api/v1/orders";
+  const token = localStorage.getItem("token");
+  
+  const randomAmount = getRandomInt(10_000); // para generar producto aleatorio
+ 
+  orderData = {
+    purchaseDate: "2023-09-04T12:06:31",
+    totalAmount: randomAmount,
+    customer: { id: 1 },
+  };
 
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(orderData),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const newOrder = await response.json();
+  console.log(newOrder);
+};
 
+/**
+ * Se requiere tener un token
+ */
+const getOrders = async () => {
+  const url = "http://localhost:8080/api/v1/orders";
+  const token = localStorage.getItem("token");
+  const response = await fetch(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  console.log(response.status);
+  const orders = await response.json();
+  console.log(orders);
+};
 
-const calculateAge = ( {birthdate : birthdateStr }  )=>{
-    console.log(birthdateStr); // string  1996-11-30 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+};
 
-    // Date entrega los milisegundos transcurridos desde el 01/01/1970
-    const today = new Date(); // Crear un objeto con la fecha en curso
-    const birthdate = new Date( birthdateStr ); // Objeto con la fecha 1996-11-30
-
-    const difference = today - birthdate;
-    console.log ( difference ); // 842810738302 milisegundos de diferencia
-    const age = new Date( difference ); // Sun Sep 15 1996 13:09:51 GMT-0500
-    console.log( age );
-    console.log(`Years: ${age.getFullYear() - 1970} `); //  1996 -1970 = 26
-    console.log(`Months: ${age.getMonth() } `); //  
-    console.log(`Days: ${age.getDate() } `); //  
-    return {
-        year: age.getFullYear() - 1970 ,
-        month: age.getMonth() ,
-        day: age.getDate() ,
-    }
-
-}
-
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
 const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
+  const wrapper = document.createElement("div");
   wrapper.innerHTML = [
     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
     `   <div>${message}</div>`,
     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+    "</div>",
+  ].join("");
 
-  alertPlaceholder.append(wrapper)
-}
+  alertPlaceholder.append(wrapper);
+};
